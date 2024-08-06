6f584d96dfc9669ab18e3cfe6b6912ad741f5682
vNussem <125178941+vNussem@users.noreply.github.com>
2024-08-06 21:07:00 +0300

    refactor useGames using ReactQuery

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -15,7 +15,7 @@ export const GameGrid = ({ gameQuery }: Props) => {
 
   const skeletons = [1, 2, 3, 4, 5, 6];
 
-  if (error) return <Text>{error}</Text>;
+  if (error) return <Text>{error.message}</Text>;
 
   return (
     <SimpleGrid
@@ -29,7 +29,7 @@ export const GameGrid = ({ gameQuery }: Props) => {
             <GameCardSkeleton />
           </GameCardContainer>
         ))}
-      {data.map((game) => (
+      {data?.map((game) => (
         <GameCardContainer key={game.id}>
           <GameCard game={game} />
         </GameCardContainer>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,10 @@
+import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import useData from "./useData";
 import { Genre } from "./useGenres";
+import { CACHE_KEY_GAMES } from "../constants";
+
+import apiClient, { FetchResponse } from "../services/api-client";
 
 export interface Platform {
     id: number;
@@ -17,14 +21,31 @@ export interface Game {
     rating_top: number; 
 }
 
-const useGames = (gameQuery: GameQuery | null) => useData<Game>("/games"
-    , {params: {
-        genres: gameQuery?.genre?.id, 
-        platforms: gameQuery?.platform?.id, 
-        ordering: gameQuery?.sortOder,
-        search: gameQuery?.searchText
-      }}
-    , [gameQuery])
+
+
+const useGames = (gameQuery: GameQuery | null) => {
+    const {data, error, isLoading: loading} = useQuery<Game[], Error>({
+        queryKey: [...CACHE_KEY_GAMES, {genre: gameQuery?.genre?.id, platform: gameQuery?.platform?.id, sortOrder: gameQuery?.sortOder}],
+        queryFn: () => apiClient.get<FetchResponse<Game>>("/games", {params: {
+                    genres: gameQuery?.genre?.id, 
+                    parent_platforms: gameQuery?.platform?.id, 
+                    ordering: gameQuery?.sortOder,
+                    search: gameQuery?.searchText
+                  }}).then(res => res.data.results),
+                  staleTime: 60 * 1_000
+        });
+    
+    return {data, error, loading};
+}
+
+// const useGames = (gameQuery: GameQuery | null) => useData<Game>("/games"
+//     , {params: {
+//         genres: gameQuery?.genre?.id, 
+//         platforms: gameQuery?.platform?.id, 
+//         ordering: gameQuery?.sortOder,
+//         search: gameQuery?.searchText
+//       }}
+//     , [gameQuery])
 
 export default useGames;
 
