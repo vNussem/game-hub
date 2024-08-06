8e2a540c05641b6e71fff44c0a6af2d427b28bb0
vNussem <125178941+vNussem@users.noreply.github.com>
2024-08-06 22:15:03 +0300

    move Platform interface in usePlatforms

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,10 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import { CACHE_KEY_PLATFORMS } from "../constants";
 import apiClient from "../services/api-client";
-import { Platform } from "./useGames";
-
 import { FetchResponse } from "../services/api-client";
 
+export interface Platform {
+    id: number;
+    name: string;
+    slug: string;
+}
 
 const usePlatform = () => useQuery({
     queryKey: CACHE_KEY_PLATFORMS,
